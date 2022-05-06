b2520539b3098a75561181aadd8e9f71bc5ab5d9
Dmitry Petrov <petrovdmitrys@yandex.ru>
2022-05-06 23:41:43 +0500

    Added colors.

---
+++ lib/constants.dart
@@ -0,0 +1,36 @@
+// colors
+import 'package:flutter/material.dart';
+
+// accent colors
+const Color colorAccentBlack = Color(0xFF283244);
+const Color colorAccentDarkBlue = Color(0xFF575FCC);
+const Color colorAccentLightBlue = Color(0xFF739EF1);
+const Color colorAccentGold = Color(0xFFFABF48);
+const Color colorAccentSoftGold = Color(0xFFFEF2DA);
+const Color colorAccentRed = Color(0xFFFF6D3B);
+const Color colorAccentGreen = Color(0xFF8FD8B5);
+
+// text colors
+const Color colorText = Color(0xFF283244);
+const Color colorTextContrast = Color(0xFFFFFFFF);
+const Color colorTextSecondary = Color(0xFF74767A);
+const Color colorLink = Color(0xFF575FCC);
+
+// icon colos
+const Color colorIcon = Color(0xFF283244);
+const Color colorIconContrast = Color(0xFFFFFFFF);
+const Color colorIconSecondary = Color(0xFF919399);
+const Color colorIconAccent = Color(0xFFFF6D3B);
+
+// controls colors
+const Color colorButton = Color(0xFF283244);
+const Color colorButtonDisable = Color(0xFFA0A2A8);
+const Color colorButtonSecondary = Color(0xFFECEDF0);
+const Color colorInput = Color(0xFFF3F3F5);
+const Color colorInputContent = Color(0xFF74767A);
+const Color colorInputActive = Color(0xFF739EF1);
+const Color colorInputError = Color(0xFFFF6D3B);
+
+// divider & border colors
+const Color colorBorder = Color(0xFFE0E1E6);
+const Color colorDivider = Color(0xFFECEDF0);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,58 +10,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.white.withOpacity(0),
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+      child: const MaterialApp(
+        title: 'SISBI',
+        // theme: getTheme(),
+        home: Scaffold(),
       ),
     );
   }
